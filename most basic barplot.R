03f053d6721d31c249c18dbb3b5789a6a747f485
yli-qlc <85208491+yli-qlc@users.noreply.github.com>
2021-07-06 12:58:45 -0400

    Add files via upload

---
+++ most basic barplot.R
@@ -0,0 +1,29 @@
+# Libraries
+library(tidyverse)
+
+# Create dataset
+data <- read.csv("C:\\Users\\YingLi\\Desktop\\test3.csv",header=TRUE, sep=",")
+data$id <- seq(1, nrow(data))
+view (data)
+# Make the plot
+p <- ggplot(data, aes(x=as.factor(id), y=Value)) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
+  
+  # This add the bars with a blue color
+  geom_bar(stat="identity", fill=alpha("purple", 0.3)) +
+  
+  # Limits of the plot = very important. The negative value controls the size of the inner circle, the positive one is useful to add size over each bar
+  ylim(-100,120) +
+  
+  # Custom the theme: no axis title and no cartesian grid
+  theme_minimal() +
+  theme(
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    panel.grid = element_blank(),
+    plot.margin = unit(rep(-2,4), "cm")     # This remove unnecessary margin around plot
+  ) +
+  
+  # This makes the coordinate polar instead of cartesian.
+  coord_polar(start = 0)
+
+p
